4521db98c8478d60cf879c8d51623bc403469f18
Ayush <unitedestates.ayush10@gmail.com>
2023-02-19 20:59:27 +0530

    password goes here

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./Login.module.css";
+import PasswordInput from "./passwordInput";
 import background from "../../static_files/BG_Main_Dark.svg";
 import login from "../../static_files/login.png";
 const Login = () => {
@@ -8,29 +9,28 @@ const Login = () => {
       <div className={styles.container1}>
         <div className={styles.container2}>
           <form className={styles.form}>
-            <img
-              className={styles.image}
-              src="src\static_files\login.png"
-              alt="login"
-            />
-            <div className={styles.input1}>
+            <img className={styles.image} src={login} alt="login" />
+            <div className={styles.input}>
               <input
                 type="email"
                 placeholder="Enter your email here"
                 required
               />
             </div>
-            <div className={styles.input2}>
+
+            <PasswordInput />
+
+            {/* <div className={styles.input}>
               <input type="password" placeholder="Password goes here" />
-            </div>
+            </div> */}
             <div className={styles.forgot}>
               <a href="#">Forgot Password?</a>
             </div>
-            <button>Login</button>
+            <button>Login &rarr;</button>
             <div className={styles.noAccount}>
               <p>
                 Don't Have an account?{" "}
-                <a className={styles.register} href="#">
+                <a className={styles.register} href="/Signup">
                   Register
                 </a>
               </p>
