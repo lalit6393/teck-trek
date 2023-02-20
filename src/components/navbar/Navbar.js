badd2a5e587d6757a7b3c21c0c72693ad2984e19
lalit6393 <amanengineer132@gmail.com>
2023-02-20 22:13:15 +0530

    20/02/2023|22:13

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,38 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import navStyle from "./style.module.css";
-import { Avatar } from "@mui/material";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Avatar, Drawer } from "@mui/material";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import appNameImg from "../../static_files/tecktrek.svg";
+import coolicon from "../../static_files/coolicon.svg";
 import logoutIcon from "../../static_files/logoutIcon.svg";
+import crossIcon from "../../static_files/crossIcon.svg";
+import volume_up from "../../static_files/volume_up.svg";
 
 const Navbar = () => {
-  
+  const location = useLocation();
   const navigate = useNavigate();
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const backgroundBlur = {
+    background:
+      "radial-gradient(80.95% 49.95% at 50% 100.57%, #08ac70 0%, rgba(14, 174, 115, 0) 100%)",
+    filter: "blur(14px)",
+  };
+
+  const handleClickOpen = () => {
+    setOpenDrawer(true);
+  };
+
+  const handleClose = () => {
+    setOpenDrawer(false);
+  };
 
   return (
     <div className={navStyle.outermostDiv}>
       <div className={navStyle.navbar}>
         <div className={navStyle.leftDiv}>
-          <div className={navStyle.navIcon}></div>
-          <div className={navStyle.appName} onClick={() => navigate('/')}>
-            <img src={appNameImg} alt={"TeckTrek"} height="20vh"></img>
+          <div className={navStyle.appName} onClick={() => navigate("/")}>
+            <img src={appNameImg} alt={"TeckTrek"} height="23px"></img>
           </div>
         </div>
-        <div style={{ flex: "1" }} />
         <div className={navStyle.rightDiv}>
+          <div style={{ flex: "1" }} />
           <div className={navStyle.navigation}>
             <ul>
               <li>
-                <a className={navStyle.anchor} href="/dashboard">Dashboard</a>
-                <div className={navStyle.backgroundBlur}>Dashboard</div>
+                <a
+                  style={
+                    (location.pathname.match("/dashboard") ||
+                      location.pathname.match("/")) &&
+                    !location.pathname.match("/rules") &&
+                    !location.pathname.match("/leaderboard")
+                      ? { borderBottom: "0.6rem solid rgb(34, 139, 34)" }
+                      : null
+                  }
+                  className={navStyle.anchor}
+                  href="/dashboard"
+                >
+                  Dashboard
+                </a>
+                <div
+                  style={
+                    (location.pathname.match("/dashboard") ||
+                      location.pathname.match("/")) &&
+                    !location.pathname.match("/rules") &&
+                    !location.pathname.match("/leaderboard")
+                      ? backgroundBlur
+                      : null
+                  }
+                  className={navStyle.backgroundBlur}
+                >
+                  Dashboard
+                </div>
               </li>
               <li>
-                <a className={navStyle.anchor} href="/rules ">Rules</a>
-                <div className={navStyle.backgroundBlur}>Rules</div>
+                <a
+                  style={
+                    location.pathname.match("/rules")
+                      ? { borderBottom: "0.6rem solid rgb(34, 139, 34)" }
+                      : null
+                  }
+                  className={navStyle.anchor}
+                  href="/rules"
+                >
+                  Rules
+                </a>
+                <div
+                  style={
+                    location.pathname.match("/rules") ? backgroundBlur : null
+                  }
+                  className={navStyle.backgroundBlur}
+                >
+                  Rules
+                </div>
               </li>
               <li>
-                <a className={navStyle.anchor} href="/leaderboard">Leaderboard</a>
-                <div className={navStyle.backgroundBlur}>Leaderboard</div>
+                <a
+                  style={
+                    location.pathname.match("/leaderboard")
+                      ? { borderBottom: "0.6rem solid rgb(34, 139, 34)" }
+                      : null
+                  }
+                  className={navStyle.anchor}
+                  href="/leaderboard"
+                >
+                  Leaderboard
+                </a>
+                <div
+                  style={
+                    location.pathname.match("/leaderboard")
+                      ? backgroundBlur
+                      : null
+                  }
+                  className={navStyle.backgroundBlur}
+                >
+                  Leaderboard
+                </div>
               </li>
             </ul>
           </div>
@@ -44,21 +121,122 @@ const Navbar = () => {
                 bgcolor: "grey",
                 cursor: "pointer",
                 fontWeight: "bold",
-                width: "5vh",
-                height: "5vh",
+                width: "3.8rem",
+                height: "3.8rem",
                 fontFamily: "Avenir",
-                fontSize:"2vh"
+                fontSize: "1.4rem",
               }}
               src="#"
             >
               {"White fang".slice(0, 1)}
             </Avatar>
             <div className={navStyle.icons}>
-              <img src={logoutIcon} alt="logoutIcon" width={"20px"}></img>
+              <img src={logoutIcon} alt="logoutIcon"></img>
             </div>
           </div>
         </div>
+        <div className={navStyle.hamburger}>
+          <img
+            onClick={handleClickOpen}
+            src={coolicon}
+            alt="hamburger"
+            width={"20px"}
+          ></img>
+        </div>
       </div>
+      <Drawer
+        className={navStyle.drawerComp}
+        anchor={"top"}
+        open={openDrawer}
+        onClose={handleClose}
+      >
+        <>
+          <div className={navStyle.navbar}>
+            <div className={navStyle.leftDivDrawer}>
+              <div className={navStyle.userProfileOnPhone}>
+                <Avatar
+                  sx={{
+                    bgcolor: "grey",
+                    cursor: "pointer",
+                    fontWeight: "bold",
+                    width: "3.8rem",
+                    height: "3.8rem",
+                    fontFamily: "Avenir",
+                    fontSize: "1.4rem",
+                  }}
+                  src="#"
+                >
+                  {"White fang".slice(0, 1)}
+                </Avatar>
+                <p>
+                  {"White fang"
+                    .split(" ")
+                    .map(
+                      (w) => w[0].toUpperCase() + w.substring(1).toLowerCase()
+                    )
+                    .join(" ")}
+                </p>
+              </div>
+            </div>
+            <div style={{ flex: "1" }} />
+            <div className={navStyle.crossIconDrawer}>
+              <img
+                onClick={handleClose}
+                src={crossIcon}
+                alt="hamburger"
+                width={"13px"}
+              ></img>
+            </div>
+          </div>
+          <div className={navStyle.drawerContent}>
+            <ul>
+              <li
+                style={
+                  (location.pathname.match("/dashboard") ||
+                    location.pathname.match("/")) &&
+                  !location.pathname.match("/rules") &&
+                  !location.pathname.match("/leaderboard")
+                    ? { background: "#08AC70" }
+                    : null
+                }
+                onClick={() => navigate('/dashboard')}
+              >
+                Dashboard
+              </li>
+              <li
+                style={
+                  location.pathname.match("/rules")
+                    ? { background: "#08AC70" }
+                    : null
+                }
+                onClick={() => navigate('/rules')}
+              >
+                Rules
+              </li>
+              <li
+                style={
+                  location.pathname.match("/leaderboard")
+                    ? { background: "#08AC70" }
+                    : null
+                }
+                onClick={() => navigate('/leaderboard')}
+              >
+                Leaderboard
+              </li>
+              <li>
+                <a href="/login">Logout</a>
+              </li>
+              <li>
+                <img src={volume_up} alt="sound"></img>
+              </li>
+              <li>
+                <span>Designed & Developed by:</span>
+                <span>Nibble Computer Society</span>
+              </li>
+            </ul>
+          </div>
+        </>
+      </Drawer>
       <Outlet />
     </div>
   );
